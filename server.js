b96ffa5e1d97c7bf454a452c297b28f3effb1374
awallathome <adam.wallis@me.com>
2017-09-25 23:52:37 -0400

    mostly attempts to compare score arrays in the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var survey = require('./app/public/survey.html');
+var friends = require("./app/data/friends.js");
 
 var app = express();
 
@@ -21,7 +23,21 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require("./app/routing/apiroutes");
 require("./app/routing/htmlroutes");
 
+var findFriend = function () {
+
+};
+
 //to make sure the server we set up above actually runs
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
+
+// HERE, I AM PUTTING TOGETHER CODE FOR THE FUNCTION THAT MATCHES THE USER WITH THE BEST MATCH. IT IS MY THIRD ATTEMPT TO DO SO...
+function compareScores(){
+  for (i = 0; i > friends.length; i++) {
+    var friendOnDeck = friends[i].scores;
+    var sum = 0;sum += objectOnDeck[i];
+
+  }
+
+};

d4a4ea2416f1461b463614b2269742213d5f6840
awallathome <adam.wallis@me.com>
2017-09-26 18:33:16 -0400

    final attempt to compare arrays for new friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,7 @@
 
 
-{
+module.exports = [
+  {
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
@@ -15,4 +16,21 @@
       4,
       1
     ]
-}
+  },
+  {
+  "name":"Kevin",
+  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "scores":[
+      3,
+      2,
+      9,
+      2,
+      2,
+      2,
+      2,
+      5,
+      4,
+      1
+    ]
+  }
+];
--- server.js
+++ server.js
@@ -20,24 +20,61 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+
 require("./app/routing/apiroutes");
 require("./app/routing/htmlroutes");
 
-var findFriend = function () {
+// var findFriend = function () {
 
-};
+// };
 
 //to make sure the server we set up above actually runs
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT: " + PORT);
+// });
 
-// HERE, I AM PUTTING TOGETHER CODE FOR THE FUNCTION THAT MATCHES THE USER WITH THE BEST MATCH. IT IS MY THIRD ATTEMPT TO DO SO...
-function compareScores(){
-  for (i = 0; i > friends.length; i++) {
-    var friendOnDeck = friends[i].scores;
-    var sum = 0;sum += objectOnDeck[i];
+// var results = [];
 
+var user = {
+  name: "Thomas",
+  photo: "someurl.com",
+  scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+  ]
+}
+
+function findBestFriend(user, friends) {
+  var bestFriend = null;
+  for (var i = 0; i < friends.length; i++) {
+    var currentFriend = friends[i];
+    var sum = 0;
+
+    for (var j = 0; j < currentFriend.scores.length; j++) {
+      var currentFriendScore = currentFriend.scores[j];
+
+      for (var k = 0; k < user.scores.length; k++) {
+        var currentUserScore = user.scores[k];
+        sum += Math.abs(currentFriendScore - currentUserScore);
+      }
+      if (bestFriend === null || sum <= bestFriend.score) {
+        bestFriend = { friend: currentFriend, score: sum };
+      }
+      // do something to check if we have a new best friend
+    }
+    
   }
+  return bestFriend;
+}
 
-};
+var bestFriend = findBestFriend(user, friends);
+console.log(bestFriend);
+
